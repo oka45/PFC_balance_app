f1f84e88aad244d1ea317b7c414ddb634e014d4b
oka45 <soccer45.kenichilove@gmail.com>
2021-01-15 23:35:15 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+  def setup
+    @base_title = "PFCバランサー"
+  end
+
+  test "should get root(ルーティング、ルートのテスト)" do
+    get root_url
     assert_response :success
   end
+
+  test "should get home(ホームページのテスト)" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "TOP|#{@base_title}"
+  end
+
+  test "shold get about"do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "使い方|#{@base_title}"
+  end
+
 end
