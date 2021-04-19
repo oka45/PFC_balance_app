8ae54e09fe53f90f61b017fd331a50633897a66e
oka45 <soccer45.kenichilove@gmail.com>
2021-04-19 10:44:59 +0900

    Implementation docker

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,18 @@ FROM ruby:2.6.6
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
     && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs yarn
+WORKDIR /myapp
+COPY Gemfile /myapp/Gemfile
+COPY Gemfile.lock /myapp/Gemfile.lock
 
-WORKDIR /PFC_balance_app
-
-COPY Gemfile /PFC_balance_app/Gemfile
-COPY Gemfile.lock /PFC_balance_app/Gemfile.lock
-
-RUN gem install bundler
 RUN bundle install
+COPY . /myapp
 
-ADD . /PFC_balance_app
+COPY entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
+EXPOSE 3000
+
+CMD ["rails", "server", "-b", "0.0.0.0"]
+
+ADD . /myapp
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_04_01_021324) do
 
-  create_table "foods", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "foods", charset: "utf8", force: :cascade do |t|
     t.string "food_name"
     t.decimal "protein", precision: 6, scale: 1
     t.decimal "carbohydrate", precision: 6, scale: 1
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_021324) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
